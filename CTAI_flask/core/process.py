db7b0ff63814267e645425dc4ae135351d9825c2
zzw <3279527604@qq.com>
2024-03-06 21:22:48 +0800

    Initial commit

---
+++ CTAI_flask/core/process.py
@@ -0,0 +1,55 @@
+import os
+
+import SimpleITK as sitk
+import cv2
+import numpy as np
+import torch
+
+
+# 归一化
+def data_in_one(inputdata):
+    if not inputdata.any():
+        return inputdata
+    inputdata = (inputdata - inputdata.min()) / (inputdata.max() - inputdata.min())
+    return inputdata
+
+
+def pre_process(data_path):
+    """预处理：首先读取dcm文件，然后对图像数据进行处理，包括选择感兴趣区域（ROI）、归一化、转换为PyTorch张量等。
+    最后，将处理后的图像数据保存为PNG格式的图片文件。函数返回处理后的图像数据以及文件名"""
+    global test_image, test_mask
+    image_list, mask_list, image_data, mask_data = [], [], [], []
+
+    image = sitk.ReadImage(data_path)
+    image_array = sitk.GetArrayFromImage(image)
+
+    ROI_mask = np.zeros(shape=image_array.shape)
+    ROI_mask_mini = np.zeros(shape=(1, 160, 100))
+    ROI_mask_mini[0] = image_array[0][270:430, 200:300]
+    ROI_mask_mini = data_in_one(ROI_mask_mini)
+    ROI_mask[0][270:430, 200:300] = ROI_mask_mini[0]
+    test_image = ROI_mask
+    image_tensor = torch.from_numpy(ROI_mask).float().unsqueeze(1)
+    # print(image_tensor.shape)
+    image_data.append(image_tensor)
+    file_name = os.path.split(data_path)[1].replace('.dcm', '')
+
+    # 转为图片写入image文件夹
+    image_array = image_array.swapaxes(0, 2)
+    image_array = np.rot90(image_array, -1)
+    image_array = np.fliplr(image_array).squeeze()
+    # ret, image_array = cv2.threshold(image_array, 150, 255, cv2.THRESH_BINARY)
+    cv2.imwrite(f'./tmp/image/{file_name}.png', image_array, (cv2.IMWRITE_PNG_COMPRESSION, 0))
+
+    return image_data, file_name
+
+
+def last_process(file_name):
+    """对模型输出的分割结果进行后处理。它读取保存的原始图像文件和分割结果掩码文件，然后使用OpenCV的cv2.findContours函数找到掩码中的轮廓，
+    并在原始图像上绘制这些轮廓，最后将结果保存为PNG格式的图片文件。"""
+    image = cv2.imread(f'./tmp/image/{file_name}.png')
+    mask = cv2.imread(f'./tmp/mask/{file_name}_mask.png', 0)
+    # thresh, contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    draw = cv2.drawContours(image, contours, -1, (0, 255, 0), 2)
+    cv2.imwrite(f'./tmp/draw/{file_name}.png', draw)
